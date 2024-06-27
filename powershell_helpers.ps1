17825fcaf8585ce794e9b19ab700aada5a485ef3
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-06-28 00:49:49 +0100

    error in powershell_helpers

--- powershell_helpers.ps1
+++ powershell_helpers.ps1
@@ -3,13 +3,15 @@ return [PowerShellRuntimeExtensions.JsonObject]::ConvertFromJson($inputObject, $
 #PowerShellRuntimeExtensions.dll
 Add-Type -Path PowerShellRuntimeExtensions.dll
 
-function ConvertFrom-Json20([object] $inputObject) {
+function ConvertFrom-Json20([object] $inputObject)
+{
     $err = $null
     return [PowerShellRuntimeExtensions.JsonObject]::ConvertFromJson($inputObject, $false, 4, [ref]$err)
 }
-function ConvertTo-Json20([object] $inputObject, $depth = 5) {
+function ConvertTo-Json20([object] $inputObject, $depth = 5)
+{
     $ctx = New-Object PowerShellRuntimeExtensions.ConvertToJsonContext $depth, $false, $false, 'Default'
     return [PowerShellRuntimeExtensions.JsonObject]::ConvertToJson($inputObject, [ref]$ctx)
-    if ($null -eq (Get-Command 'ConvertTo-Json' -ErrorAction SilentlyContinue)) { New-Alias -Name 'ConvertTo-JSON' -Value 'ConvertTo-Json20' -Scope Global _-Force }
+    if ($null -eq (Get-Command 'ConvertTo-Json' -ErrorAction SilentlyContinue)) { New-Alias -Name 'ConvertTo-JSON' -Value 'ConvertTo-Json20' -Scope Global -Force }
     if ($null -eq (Get-Command 'ConvertFrom-Json' -ErrorAction SilentlyContinue)) { New-Alias -Name 'ConvertFrom-JSON' -Value 'ConvertFrom-JSON' -Scope Global -Force }
 }
